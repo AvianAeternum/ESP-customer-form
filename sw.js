57991e2e1349ec0c10b8591f62c3a9f7a11d1ccb
Aidan Aeternum <aidanaeternum@gmail.com>
2023-09-10 23:53:35 -0700

    Fixed some pwa stuff

--- sw.js
+++ sw.js
@@ -2,12 +2,12 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('my-cache').then((cache) => {
             return cache.addAll([
-                '/',
-                '/index.html',
-                '/manifest.json',
-                '/sw.js',
-                `/assets/icon.png`,
-                `/assets/icon-192.png`,
+                '/ESP-customer-form/',
+                '/ESP-customer-form/index.html',
+                '/ESP-customer-form/manifest.json',
+                '/ESP-customer-form/sw.js',
+                `/ESP-customer-form/assets/icon.png`,
+                `/ESP-customer-form/assets/icon-192.png`,
                 // ... add other assets and resources
             ]);
         })
